3ac90fc5b753b6ed026155b13d5c57daf947af03
Josh Pann <josh.pann@example.com>
2017-04-20 23:51:58 -0400

    need to add items to modal

--- scrapCode.js
+++ scrapCode.js
@@ -25,3 +25,191 @@ console.log(number_of_products)
 
 var number_of_products2 = document.getElementsByClassName("minicart-quantity")[0].innerText
 console.log(number_of_products)
+
+
+
+//number of products. 3 solutions
+var numberOfProducts = document.getElementsByClassName("mini-cart-container")[0].getAttribute("data-quantity")
+
+//orderTotal displayed in dollars
+var orderTotal = document.getElementsByClassName("order-value")[0].innerText
+
+//html collection
+var cartImages = document.querySelectorAll(".mini-cart-image img")
+
+//Array
+var cartImagesArray = [...cart_images]
+
+//get srcs
+var cartSrcLinks = cart_images_array.map(function(product){return product.src})
+
+
+function jsAdvert(){
+  let numberOfProducts = document.getElementsByClassName("mini-cart-container")[0].getAttribute("data-quantity")
+  let cartSrcLinks
+  let orderTotal
+  if (numberOfProducts > 0 ){
+    orderTotal = document.getElementsByClassName("order-value")[0].innerText
+    cartSrcLinks = collectImageSrcs()
+
+  } else {
+    return
+  }
+  console.log(cartSrcLinks)
+  console.log(numberOfProducts)
+  console.log(orderTotal)
+}
+
+
+function collectImageSrcs(){
+  let cartImages = document.querySelectorAll(".mini-cart-image img")
+  let cartImagesArray = [...cartImages]
+  return cartImagesArray.map(function(product){return product.src})
+  //.map returns a new array, but you still need explicit return to return this new array as the value of collectImageSrcs()
+}
+
+function popup(){
+
+}
+
+jsAdvert()
+
+//approach 1
+// var modal = (function(){
+//   var
+//   method = {},
+//   $overlay = $('<div id="overlay"></div>')
+//   $modal = $('<div id="modal"></div>')
+//   $content = $('<div id="content">No Content Yet!</div>')
+//   $close = $('<a id="close" href="#">close</a>')
+//
+//   //css
+//   $overlay.css({
+//     position: fixed;
+//     top: 0;
+//     left: 0;
+//     width: 100%;
+//     height: 100%;
+//     background: #000;
+//     opacity: 0.5;
+//     filter: alpha(opacity=50);
+//   })
+//
+//   $modal.css({
+//     position:absolute;
+//     background:url(tint20.png) 0 0 repeat;
+//     background:rgba(0,0,0,0.2);
+//     border-radius:14px;
+//     padding:8px;
+//   })
+//
+//   $content.css({
+//     border-radius:8px;
+//     background:#fff;
+//     padding:20px;
+//   })
+//
+//
+//   $modal.hide()
+//   $overlay.hide()
+//   $modal.append($content, $close)
+//
+//   $(document).ready(function(){
+//     $('body').append($overlay, $modal);
+//   });
+//
+//   // Center the modal in the viewport
+//   method.center = function () {
+//   var top, left;
+//
+//   top = Math.max($(window).height() - $modal.outerHeight(), 0) / 2;
+//   left = Math.max($(window).width() - $modal.outerWidth(), 0) / 2;
+//
+//     $modal.css({
+//       top:top + $(window).scrollTop(),
+//       left:left + $(window).scrollLeft()
+//     });
+//   };
+//
+//   // Open the modal
+//   method.open = function (settings) {
+//     $content.empty().append(settings.content);
+//
+//     $modal.css({
+//       width: settings.width || 'auto',
+//       height: settings.height || 'auto'
+//     })
+//
+//     method.center();
+//
+//     $(window).bind('resize.modal', method.center);
+//
+//     $modal.show();
+//     $overlay.show();
+//   };
+//
+//   // Close the modal
+//   method.close = function () {
+//     $modal.hide();
+//     $overlay.hide();
+//     $content.empty();
+//     $(window).unbind('resize.modal');
+//   };
+//
+//   return method;
+// }());
+
+
+//approach 2
+document.body.onload = addElement;
+
+function addElement () {
+
+  var $bounceOverlay = $('<div id="overlay"></div>');
+  var $bounceModal = $('<div id="modal"></div>');
+  var $modalContent = $('<div id="content">No JavaScript Yet</div>');
+  var $close = $('<a id="close" href="#">close</a>');
+
+  $bounceModal.append($modalContent, $close)
+
+  $bounceOverlay.css({
+    position: "fixed",
+    top: "0",
+    left: "0",
+    width: "100%",
+    height: "100%",
+    background: "black",
+    opacity: "0.5",
+    filter: "alpha(opacity=50)",
+  })
+
+  $bounceModal.css({
+    position: "absolute",
+    background: "url(tint20.png) 0 0 repeat",
+    width: "500px",
+    height: "200px",
+    "border-radius": "14px",
+    padding: "8px",
+  })
+
+  $modalContent.css({
+    "border-radius": "8px",
+    "text-align": "center",
+    background: "#fff",
+    padding: "20px",
+  })
+
+  jQuery.fn.center = function () {
+      this.css("position","absolute");
+      this.css("top", Math.max(0, (($(window).height() - $(this).outerHeight()) / 2) +
+                                                  $(window).scrollTop()) + "px");
+      this.css("left", Math.max(0, (($(window).width() - $(this).outerWidth()) / 2) +
+                                                  $(window).scrollLeft()) + "px");
+      return this;
+  }
+
+  $bounceModal.center()
+  $(document).ready(function(){
+    $('body').append($bounceOverlay, $bounceModal);
+  });
+}
--- script.js
+++ script.js
@@ -1,35 +1,20 @@
 ///Reqs: Extract number of items in cart, Cart Total, Cart images
 
-//number of products. 3 solutions
-var numberOfProducts = document.getElementsByClassName("mini-cart-container")[0].getAttribute("data-quantity")
 
-//orderTotal displayed in dollars
-var orderTotal = document.getElementsByClassName("order-value")[0].innerText
-
-//html collection
-var cartImages = document.querySelectorAll(".mini-cart-image img")
-
-//Array
-var cartImagesArray = [...cart_images]
-
-//get srcs
-var cartSrcLinks = cart_images_array.map(function(product){return product.src})
-
+document.body.onload = jsAdvert
 
 function jsAdvert(){
   let numberOfProducts = document.getElementsByClassName("mini-cart-container")[0].getAttribute("data-quantity")
   let cartSrcLinks
-  let orderTotal
   if (numberOfProducts > 0 ){
-    orderTotal = document.getElementsByClassName("order-value")[0].innerText
     cartSrcLinks = collectImageSrcs()
-
+    addModal()
   } else {
     return
   }
   console.log(cartSrcLinks)
   console.log(numberOfProducts)
-  console.log(orderTotal)
+
 }
 
 
@@ -40,8 +25,96 @@ function collectImageSrcs(){
   //.map returns a new array, but you still need explicit return to return this new array as the value of collectImageSrcs()
 }
 
-function popup(){
-  
-}
+function addModal() {
+  var method = {}
+  var $bounceOverlay = $('<div id="overlay"></div>');
+  var $bounceModal = $('<div id="modal"></div>');
+  var $modalContent = $('<div id="content">No JavaScript YetNo JavaScript YetNo JavaScript YetNo JavaScript YetNo JavaScript YetNo JavaScript YetNo JavaScript YetNo JavaScript YetNo JavaScript YetNo JavaScript YetNo JavaScript YetNo JavaScript YetNo JavaScript YetNo JavaScript YetNo JavaScript YetNo JavaScript YetNo JavaScript YetNo JavaScript YetNo JavaScript YetNo JavaScript YetNo JavaScript YetNo JavaScript YetNo JavaScript YetNo JavaScript YetNo JavaScript YetNo JavaScript YetNo JavaScript YetNo JavaScript YetNo JavaScript YetNo JavaScript YetNo JavaScript YetNo JavaScript YetNo JavaScript YetNo JavaScript YetNo JavaScript YetNo JavaScript YetNo JavaScript YetNo JavaScript YetNo JavaScript YetNo JavaScript Yet</div>');
+  var $close = $('<button id="closeBtn">Continue Shopping</button>');
+  var $checkout = $('<a href="https://marmot.com/cart" id="checkoutbtn">Checkout</a>');
+  var $modalSubtotal = $('<div id="modalSubtotal"></div>');
 
-jsAdvert()
+  $bounceOverlay.css({
+    position: "fixed",
+    top: "0",
+    left: "0",
+    width: "100%",
+    height: "100%",
+    background: "black",
+    opacity: "0.5",
+    filter: "alpha(opacity=50)",
+  })
+
+  $bounceModal.css({
+    background: " 0 0 repeat",
+    width: "600px",
+    "border-radius": "14px",
+    padding: "8px",
+  })
+
+  $modalContent.css({
+    "border-radius": "8px",
+    "text-align": "center",
+    background: "#fff",
+    padding: "20px",
+  })
+
+  $checkout.css({
+    appearance: "button",
+    "text-decoration": "none",
+    color: "initial",
+  })
+
+  jQuery.fn.center = function ()
+  {
+      this.css("position","fixed");
+      this.css("top", ($(window).height() / 2) - (this.outerHeight() / 2));
+      this.css("left", ($(window).width() / 2) - (this.outerWidth() / 2));
+      return this;
+  }
+
+  $bounceModal.center();
+    $(window).resize(function(){
+      $bounceModal.center();
+    });
+
+//Append content container div to Modal
+  $bounceModal.append($modalContent)
+
+
+//Append Subtotal to Content
+  let orderTotal = document.getElementsByClassName("order-value")[0].innerText
+  console.log(orderTotal)
+  $modalSubtotal.text(function() {
+    return "Subtotal: " + orderTotal;
+  });
+  $modalContent.append($modalSubtotal)
+
+//Append Buttons
+  $modalContent.append($close)
+  $modalContent.append($checkout)
+
+  $close.click(function(e){
+    e.preventDefault();
+    $bounceModal.hide();
+    $bounceOverlay.hide();
+    $modalContent.empty();
+    $(window).unbind('resize.modal')
+  });
+
+
+  $(document).ready(function(){
+    $('body').append($bounceOverlay, $bounceModal);
+  });
+  $bounceModal.hide()
+  $bounceOverlay.hide()
+
+  $(window).scroll(function(){
+    var wintop = $(window).scrollTop(), docheight = $(document).height(), winheight = $(window).height();
+    var  scrolltrigger = 0.90
+    if  ((wintop/(docheight-winheight)) > scrolltrigger) {
+      $bounceModal.show()
+      $bounceOverlay.show()
+     }
+   })
+}
